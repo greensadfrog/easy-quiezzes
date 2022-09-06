d96378d497af6b902ad37407d69ea59310fd9083
greensadfrog <deanwenchester13@gmail.com>
2022-09-06 21:27:31 +0300

    add allowed host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'easy-quizzes-api.herokuapp.com',
-                 'easy-quizzes-api-stage.herokuapp.com'
+                 'easy-quizzes-api-stage.herokuapp.com',
                  ]
 
 CORS_ALLOWED_ORIGINS = [
